e8860c86fd3694df6facc12ffd00254fe1631493
Kasiah <snipercorgii@gmail.com>
2017-05-18 12:00:58 -0700

    Added Scripts for Updated Utility Based AI

---
+++ Assets/Scripts/AI/NEW_Agent/AgentBrain.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBrain : MonoBehaviour
+{
+    // Calculate Task Based on Utilities, Holds array of Tasks with their weights calculated when a new task is needed.
+    // Utilities are added together and applied to a Curve to give smoother transitions.
+
+}
