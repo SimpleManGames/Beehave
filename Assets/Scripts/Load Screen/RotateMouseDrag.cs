409ffa924902c363b167ba965adccde0d14b8507
Riley <simplemangames@gmail.com>
2017-05-26 11:13:32 -0700

    Added a few comments to the load screening interaction scripts

--- Assets/Scripts/Load Screen/RotateMouseDrag.cs
+++ Assets/Scripts/Load Screen/RotateMouseDrag.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class RotateMouseDrag : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("How fast the models will spin")]
     private float sensitivity;
 
+    // Stores the position of the first mouse click
     private Vector3 mousePosition;
+    // Stores the difference between the first mouse click and were the mouse is currently
     private Vector3 mouseOffset;
 
+    // The new rotation vector of the object
     private Vector3 rotation;
+    // If we have clicked and held
     private bool isRotating;
 
     private void Start()
@@ -20,14 +25,19 @@ public class RotateMouseDrag : MonoBehaviour
 
     private void Update()
     {
+        // If we've clicked and held
         if (isRotating)
         {
+            // Calc the difference
             mouseOffset = (Input.mousePosition - mousePosition);
 
+            // Apply diff to the rotation vector
             rotation.y = -(mouseOffset.x + mouseOffset.y) * sensitivity;
 
+            // Act on the rotation
             transform.Rotate(rotation);
 
+            // Set are ref to the were the mouse is at
             mousePosition = Input.mousePosition;
         }
     }
