ff3643520a6c818d236222ef179eb13c7713d219
Riley <simplemangames@gmail.com>
2017-05-26 10:32:03 -0700

    Fixed error in LoadingScreen

---
+++ Assets/Scripts/Load Screen/ModelWheel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ModelWheel : MonoBehaviour
+{
+    private List<GameObject> modelsOnWheel = new List<GameObject>();
+    private List<Vector3> points = new List<Vector3>();
+
+    private void Start()
+    {
+        modelsOnWheel = new List<GameObject>();
+        foreach (Transform child in transform)
+            modelsOnWheel.Add(child.gameObject);
+
+        points = PointsAroundCircle(modelsOnWheel.Count, 3, transform.position);
+
+        for (int i = 0; i < points.Count; i++)
+            modelsOnWheel[i].transform.position = points[i];
+    }
+
+    private List<Vector3> PointsAroundCircle(int points, float radius, Vector3 center)
+    {
+        List<Vector3> retVal = new List<Vector3>();
+
+        if (!Application.isPlaying)
+            DebugExtensions.DebugPoint(center, Color.red, .1f, 1);
+
+        float slice = 2 * Mathf.PI / points;
+        for (int i = 0; i < points; i++)
+        {
+            float angle = slice * i;
+            float x = (radius * Mathf.Cos(angle) + center.x);
+            float z = (radius * Mathf.Sin(angle) + center.z);
+            retVal.Add(new Vector3(x, 0, z));
+        }
+
+        return retVal;
+    }
+}
---
+++ Assets/Scripts/Load Screen/RotateMouseDrag.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMouseDrag : MonoBehaviour
+{
+    [SerializeField]
+    private float sensitivity;
+
+    private Vector3 mousePosition;
+    private Vector3 mouseOffset;
+
+    private Vector3 rotation;
+    private bool isRotating;
+
+    private void Start()
+    {
+        rotation = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (isRotating)
+        {
+            mouseOffset = (Input.mousePosition - mousePosition);
+
+            rotation.y = -(mouseOffset.x + mouseOffset.y) * sensitivity;
+
+            transform.Rotate(rotation);
+
+            mousePosition = Input.mousePosition;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isRotating = true;
+        mousePosition = Input.mousePosition;
+    }
+
+    private void OnMouseUp()
+    {
+        isRotating = false;
+    }
+}
