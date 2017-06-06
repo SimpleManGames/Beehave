4eff324b75fd38d52636173230ad60593f5dfc18
Kasiah <snipercorgii@gmail.com>
2017-06-06 12:25:45 -0700

    Missed some stuff

---
+++ Assets/Scripts/AI/NEW_Agent/AgentBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AgentType { Bee, House, Storage, Plant, Food }
+
+public class AgentBase : MonoBehaviour
+{
+    //Set in the Unity Editor based on the Prefab Type
+    AgentType type;
+    public int ID { get; private set; }
+    public int currentTile { get; private set; }
+    List<AgentProperty> properties = new List<AgentProperty>();
+
+    public void Start()
+    {
+        SetProperties();
+    }
+
+    void SetProperties()
+    {
+        switch(type)
+        {
+            case AgentType.Bee:
+                break;
+        }
+    }
+
+    void GenBeeProperties()
+    {
+
+    }
+
+    
+}
