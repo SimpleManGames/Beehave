4b0b0e205424d425e3d5e5264f3ad5430329653c
Nicole Long <n.long1520@gmail.com>
2017-06-08 16:38:25 -0700

    added pausemenu and honey storage prefab

---
+++ Assets/Pauser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pauser : MonoBehaviour {
+    public GameObject pauseText;
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Time.timeScale!=0)
+            {
+                Debug.Log("pause");
+                pauseText.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else { Time.timeScale = 1; pauseText.SetActive(false); }
+        }
+	}
+}
